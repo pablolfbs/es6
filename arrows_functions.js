a1a81f8524aa1b58f1d3baeac2e8bf36533aa8ea
Pablo <pablo.fariabritto@gmail.com>
2017-10-20 18:39:46 -0200

    Criação do repositório es6

---
+++ arrows_functions.js
@@ -0,0 +1,6 @@
+var dobroDoValor = numero => {
+    let resultado = numero * 2
+    return resultado
+}
+
+console.log(dobroDoValor(7))
---
+++ variavel.js
@@ -0,0 +1,9 @@
+var serie = 'Friends'
+
+serie = 'The Walking Dead'
+
+if (true) {
+    let serie = 'Game of Thrones'
+    console.log(serie)
+}
+console.log(serie)
